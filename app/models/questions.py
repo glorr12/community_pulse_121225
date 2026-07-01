f3afe9925a376cf84e57e9178d71617fdc431e44
glorr12 <igor.fishkov1991@gmail.com>
2026-07-01 14:55:59 +0100

    created Category

--- app/models/questions.py
+++ app/models/questions.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import relationship, Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, Integer, ForeignKey
 
 from app.models import db
 
@@ -9,9 +9,11 @@ class Question(db.Model):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     text: Mapped[str] = mapped_column(String(100))
+    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'), nullable=False)
 
     statistics: Mapped['Statistics'] = relationship(back_populates='question', uselist=False)
     responses: Mapped[list['Response']] = relationship(back_populates='question')
+    category: Mapped["Category"] = relationship( back_populates='questions')
 
     def __str__(self):
         return self.text
@@ -20,4 +22,19 @@ class Question(db.Model):
         return f'id={self.id}, text={self.text}'
 
 
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str]= mapped_column(String(255), nullable=False)
+
+    questions: Mapped[list['Question']] = relationship(Question, back_populates='category')
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f'id={self.id}, name={self.name}'
 
